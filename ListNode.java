cc2e976dee385d6e4ddc400a2bc24e4762143db7
Forest-marker <2232841920@qq.com>
2019-12-01 23:13:08 +0800

    链表结点

---
+++ ListNode.java
@@ -0,0 +1,13 @@
+package 链表;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val,ListNode next){
+        this.val=val;
+        this.next=next;
+    }
+    ListNode(int val){
+        this(val,null);
+    }
+}
