4a745037ae80ff2b13ceb4d03cd9d96697269c0e
Forest-marker <2232841920@qq.com>
2020-03-31 22:38:45 +0800

    打印所有的状态

---
+++ 打印所有的状态.java
@@ -0,0 +1,10 @@
+package Thread;
+
+public class 打印所有的状态 {
+    public static void main(String[] args) {
+        Thread.State[] states=Thread.State.values();
+        for(Thread.State state:states){
+            System.out.println(state);
+        }
+    }
+}
