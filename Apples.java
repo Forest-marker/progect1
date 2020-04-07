c43d32466d3db12e23a1c75e98fcd5d334250296
Forest-marker <2232841920@qq.com>
2020-04-07 23:20:22 +0800

    牛客：删除字符

--- Apples.java
+++ Apples.java
@@ -1,9 +1,8 @@
 package DayOverDay_parctice.DayThree;
 
 import java.util.Scanner;
-
-public class Apples{
-    public static int Counts(int n){
+public class Main{
+/*public static int Counts(int n){
         int tmp=n/8;
         for(int i=tmp;i>=0;i--){
             int sum=n;
@@ -16,7 +15,6 @@ public class Apples{
                 b=sum-8*i;
                 //System.out.println(a+" "+b);
                 if(b==0){
-                   System.out.println(1);
                     return count;
                 }
                 if(b<6){
@@ -27,21 +25,56 @@ public class Apples{
                 b=b%6;
                 //System.out.println(a+" "+b);
                 if(b==0){
-                    System.out.println(2);
                     return count;
                 }
                 if(b<6){
                     break;
                 }
             }
-
+ 
         }
-        return 0;
-
+        return -1;
+ 
+    }*/
+  //方法二：因为只能6个或8个一个袋子，可以判断满足条件的一定是偶数，奇数都不成立
+  //n%8 只可能是0，2，4，6
+  //0时，恰好需要n/8个袋子
+  //2时，回溯两次，8+8+2=18/6=3 所需袋子数变成n/8-2+3=n/8+1
+  //4时，回溯一次,8+4=12/6=2,所需袋子个数变成n/8-1+2+n/8+1
+  //6时，刚好再用6个每袋的袋子，所需袋子数为n/8+1
+  /*public static int count(int n){
+    if(n%2!=0||n==10||n<6){
+      return -1;
     }
+    if(n%8==0){
+      return n/8;
+    }
+    return n/8+1;
+  }
     public static void main(String[] args){
         Scanner scanner=new Scanner(System.in);
         int n=scanner.nextInt();
-        System.out.println(Counts(n));
-    }
+        System.out.println(count(n));
+    }*/
+   
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+System.out.println(buyA(n));
+}
+public static int buyA(int n){
+int res=-1;
+int p=20;
+if(n%2==1||n<6) return -1;
+for(int i=0;i<=n/8;i++){
+for(int j=0;j<=n/6;j++){
+if(i*8+j*6==n){
+//res=i+j<p?i+j:p;
+  res=i+j;
+}
+}
+}
+return res;
+}
+ 
 }
